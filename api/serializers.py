60631aaa6cd8685cb0503f3e17f9336813b010cb
n8jja <jeremy@n8jja.com>
2021-09-04 10:05:44 -0700

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -5,6 +5,9 @@ from rest_framework import serializers
 from quacks.models import Quack
 
 class FeedSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedFields(read_only=True)
+
     class Meta:
         model = Quack
         fields = '__all__'
+
